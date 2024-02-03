c467114b31836aef9322eb1ac2c9746be4ae0deb
Lutin <tclutin01@gmail.com>
2024-02-03 05:40:49 +0500

    add: config

--- api/cmd/main.go
+++ api/cmd/main.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/tclutin/ArionURL/internal/config"
+)
 
+func main() {
+	//Initializing the config
+	cfg := config.MustLoad()
+	fmt.Println(cfg)
 }
---
+++ api/internal/config/config.go
@@ -0,0 +1,36 @@
+package config
+
+import (
+	"github.com/ilyakaznacheev/cleanenv"
+	"log"
+	"os"
+	"time"
+)
+
+type Config struct {
+	HTTPServer `yaml:"http_server"`
+}
+
+type HTTPServer struct {
+	Address string        `yaml:"address" env-default:"localhost:8080"`
+	Timeout time.Duration `yaml:"timeout" env-default:"2s"`
+}
+
+func MustLoad() *Config {
+	var config Config
+
+	path := os.Getenv("ARIONURL_CONFIG")
+	if path == "" {
+		log.Fatalln("specify the correct path to the config")
+	}
+
+	if _, err := os.ReadFile(path); err != nil {
+		log.Fatalln(err)
+	}
+
+	if err := cleanenv.ReadConfig(path, &config); err != nil {
+		log.Fatalln(err)
+	}
+
+	return &config
+}
