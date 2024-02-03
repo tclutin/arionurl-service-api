2e2b6786f7e9a5f0be8e2e683ade5f634392abd1
Lutin <tclutin01@gmail.com>
2024-02-03 15:14:43 +0500

    add: http server with graceful shutdown

--- api/cmd/main.go
+++ api/cmd/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"context"
 	"fmt"
+	"github.com/go-chi/chi/v5"
+	"github.com/tclutin/ArionURL/internal/app"
 	"github.com/tclutin/ArionURL/internal/config"
 	"github.com/tclutin/ArionURL/pkg/client/postgresql"
 	"github.com/tclutin/ArionURL/pkg/logging"
@@ -14,9 +16,15 @@ func main() {
 	cfg := config.MustLoad()
 
 	//Initializing the logger
-	logger := logging.InitSlog(cfg.Env)
+	logger := logging.MustLoadSlog(cfg.Env)
 
 	//Initial the pgxpool
 	pgxPool := postgresql.NewClient(context.Background(), os.Getenv("ARIONURL_DB"))
-	fmt.Println(logger, pgxPool)
+	fmt.Println(pgxPool)
+
+	//Initial the router
+	router := chi.NewRouter()
+
+	//Initial the app
+	app.New(cfg, logger, router).Run()
 }
---
+++ api/internal/app/app.go
@@ -0,0 +1,61 @@
+package app
+
+import (
+	"context"
+	"github.com/tclutin/ArionURL/internal/config"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+type app struct {
+	cfg        *config.Config
+	logger     *slog.Logger
+	httpServer *http.Server
+	router     http.Handler
+}
+
+func New(cfg *config.Config, logger *slog.Logger, router http.Handler) *app {
+	return &app{
+		cfg:    cfg,
+		logger: logger,
+		router: router,
+	}
+}
+
+func (a *app) Run() {
+	a.startHTTP()
+}
+
+func (a *app) startHTTP() {
+	a.logger.Info("start HTTP server", slog.String("address", a.cfg.Address))
+	a.httpServer = &http.Server{
+		Handler:      a.router,
+		WriteTimeout: a.cfg.Timeout,
+		ReadTimeout:  a.cfg.Timeout,
+	}
+
+	go func() {
+		err := a.httpServer.ListenAndServe()
+		if err != nil && err != http.ErrServerClosed {
+			a.logger.Error("the server error has occurred", slog.Any("error", err))
+			os.Exit(1)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
+	<-quit
+
+	a.logger.Info("shutting down")
+	a.shutdownHTTP()
+}
+
+func (a *app) shutdownHTTP() {
+	if err := a.httpServer.Shutdown(context.Background()); err != nil {
+		a.logger.Error("an error occurred during server shutdown", slog.Any("error", err))
+		os.Exit(1)
+	}
+}
